45e9209f81226a8fdeffd86d777d2f8521fbe2b2
Ryan Leyhe <ryanleyhe8@gmail.com>
2024-05-22 19:23:03 -0400

    Update config.py to pull credential from environment variable

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 from google.cloud import language_v1, language_v2
 
 
 class Config:
-    GOOGLE_APPLICATION_CREDENTIALS = r'C:\Users\ryanl\TextAnalysis\burnished-ember-422919-s5-337e519fbbd2.json'
+    GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
     data_folder = r"C:\Users\ryanl\TextAnalysis\YoutubeData\Youtube Data"
 
